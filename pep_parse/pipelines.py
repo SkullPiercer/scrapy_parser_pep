e3678f9eeb3dc98863c2dfc24bd2044f5f21e151
Alexandr <akatsuki_sanya@vk.com>
2024-10-02 22:50:37 +0500

    Least 2 tests. Dont working 2 files creation

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,5 +1,36 @@
+import csv
+import os
+from datetime import datetime
 
 
 class PepParsePipeline:
+    def __init__(self):
+        self.status_count = {}
+
+    def open_spider(self, spider):
+        pass
+
     def process_item(self, item, spider):
+        status = item.get('status')
+        if status:
+            if status in self.status_count:
+                self.status_count[status] += 1
+            else:
+                self.status_count[status] = 1
         return item
+
+    def close_spider(self, spider):
+        total = sum(self.status_count.values())
+        formatted_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        filename = f'results/status_summary_{formatted_time}.csv'
+
+        os.makedirs('results', exist_ok=True)
+
+        with open(filename, mode='w', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Статус', 'Количество'])
+
+            for status, count in self.status_count.items():
+                writer.writerow([status, count])
+
+            writer.writerow(['Total', total])
--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -11,9 +11,13 @@ ROBOTSTXT_OBEY = True
 current_time = datetime.datetime.now()
 formatted_time = current_time.strftime("%Y-%m-%dT%H-%M-%S")
 FEEDS = {
-    f'results/status_summary_{formatted_time}.csv': {
+    f'results/pep_{formatted_time}.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
     },
+}
+
+ITEM_PIPELINES = {
+    'pep_parse.pipelines.PepParsePipeline': 300,
 }
