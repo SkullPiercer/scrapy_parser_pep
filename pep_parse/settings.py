4dbca4ff1e5c314d89e8c20c0319a305ad3618f2
Alexandr <akatsuki_sanya@vk.com>
2024-10-02 22:32:16 +0500

    Now file is in results directory

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -9,9 +9,9 @@ NEWSPIDER_MODULE = 'pep_parse.spiders'
 ROBOTSTXT_OBEY = True
 
 current_time = datetime.datetime.now()
-formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+formatted_time = current_time.strftime("%Y-%m-%dT%H-%M-%S")
 FEEDS = {
-    f'status_summary_{formatted_time}.csv': {
+    f'results/status_summary_{formatted_time}.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
