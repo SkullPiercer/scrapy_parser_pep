846ba42aa413a41c20d39cb1a99514a881bb808f
Alexandr <akatsuki_sanya@vk.com>
2024-10-03 20:49:10 +0500

    Least 1 test

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,6 +1,3 @@
-import datetime
-
-
 BOT_NAME = 'pep_parse'
 
 SPIDER_MODULES = ['pep_parse.spiders']
@@ -8,10 +5,9 @@ NEWSPIDER_MODULE = 'pep_parse.spiders'
 
 ROBOTSTXT_OBEY = True
 
-current_time = datetime.datetime.now()
-formatted_time = current_time.strftime("%Y-%m-%dT%H-%M-%S")
+
 FEEDS = {
-    f'results/pep_{formatted_time}.csv': {
+    f'results/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
