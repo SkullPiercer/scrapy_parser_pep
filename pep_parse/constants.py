511cf7a9f0105f9511d726c897ec0c0096e55ade
Alexandr <akatsuki_sanya@vk.com>
2024-10-05 21:48:07 +0500

    Made spiders rewiev

---
+++ pep_parse/constants.py
@@ -0,0 +1,3 @@
+PARSER_NAME = 'pep'
+ALLOWED_DOMAINS = ['peps.python.org']
+START_URL = ['https://peps.python.org/']
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,12 +1,13 @@
 import scrapy
 
+from pep_parse.constants import ALLOWED_DOMAINS, PARSER_NAME, START_URL
 from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
-    name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    name = PARSER_NAME
+    allowed_domains = ALLOWED_DOMAINS
+    start_urls = START_URL
 
     def parse(self, response):
         page_links = response.css('a[href^="pep"]')
